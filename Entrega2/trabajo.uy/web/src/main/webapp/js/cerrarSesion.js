8878942bedb2e77e43f911a2fd9d92df9b21c4b9
Iván Arriola <83203492+ivan1arriola@users.noreply.github.com>
2023-09-19 14:19:47 -0300

    eliminar cerrrar sesion js

--- Entrega2/trabajo.uy/web/src/main/webapp/js/cerrarSesion.js
+++
@@ -1,5 +0,0 @@
-const cerrarSesion = () => {
-    const rutaActual = window.location.pathname;
-    const rutaIndexHTML = rutaActual.substring(0, rutaActual.lastIndexOf('/')) + "/index.html";
-    window.location.href = rutaIndexHTML;
-}
