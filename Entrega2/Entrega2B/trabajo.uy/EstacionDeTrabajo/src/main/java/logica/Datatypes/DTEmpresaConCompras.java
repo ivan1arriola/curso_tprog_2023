c1f3afb059937c7ec9a9b3a38631611fb1765bb2
MathiasRodriguezCastro <mrodriguezcastro24@gmail.com>
2023-10-04 13:54:33 -0300

    ss

--- Entrega2/Entrega2B/trabajo.uy/EstacionDeTrabajo/src/main/java/logica/Datatypes/DTEmpresaConCompras.java
+++ Entrega2/Entrega2B/trabajo.uy/EstacionDeTrabajo/src/main/java/logica/Datatypes/DTEmpresaConCompras.java
@@ -6,7 +6,7 @@ public class DTEmpresaConCompras extends DTEmpresa {
 	private HashSet<DTCompraPaquetes> compra_paquetes;
 	 
 	public DTEmpresaConCompras(String nick, String mail, String ap, String nombre, byte[] img, String desc, String URL, HashSet<DTOfertaLaboral> ols, HashSet<DTCompraPaquetes> dtcp) {
-		super(nick, mail, ap, nombre, img, desc, URL, ols);
+		super(nick, mail, ap, nombre, desc, URL, ols, img);
 		compra_paquetes = dtcp;
 	}
 	  
