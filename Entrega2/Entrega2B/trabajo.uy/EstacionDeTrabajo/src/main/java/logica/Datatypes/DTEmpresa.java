7140ac3e83060f9c004a8247151fb86167583ae2
Ivan Arriola <ivan.arriola@fing.edu.uy>
2023-10-04 12:34:52 -0400

    constructor ahora acepta ofertaslaborales

--- Entrega2/Entrega2B/trabajo.uy/EstacionDeTrabajo/src/main/java/logica/Datatypes/DTEmpresa.java
+++ Entrega2/Entrega2B/trabajo.uy/EstacionDeTrabajo/src/main/java/logica/Datatypes/DTEmpresa.java
@@ -1,6 +1,7 @@
 package main.java.logica.Datatypes;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 public class DTEmpresa extends DTUsuario {
@@ -10,14 +11,16 @@ public class DTEmpresa extends DTUsuario {
     private Set<DTOfertaLaboral> ofertasLaborales; 
 
 
-    public DTEmpresa(String nickname, String correo_electronico, String apellido, String nombre, String descripcion, String url, byte[] imagen) {
+    public DTEmpresa(String nickname, String correo_electronico, String apellido, String nombre, String descripcion, String url, Set<DTOfertaLaboral> dtOfertas, byte[] imagen) {
         super(nickname, correo_electronico, apellido, nombre, imagen);
         this.descripcion = descripcion;
         this.url = url;
-        this.ofertasLaborales = new HashSet<>(); // Inicializa el conjunto de ofertas laborales
+        this.ofertasLaborales = dtOfertas;
     }
     
-    public Set<DTOfertaLaboral> getOfertasLaborales() {
+
+
+	public Set<DTOfertaLaboral> getOfertasLaborales() {
         return ofertasLaborales;
     }
 
