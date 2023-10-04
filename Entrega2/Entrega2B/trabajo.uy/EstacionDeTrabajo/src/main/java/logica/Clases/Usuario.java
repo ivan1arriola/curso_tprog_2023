a1861872df5188576a1a9a0b63986d8cdbe881f2
Luciano Capretti <luciano.capretti@fing.edu.uy>
2023-10-04 13:40:23 -0300

    Usuario terminado

--- Entrega2/Entrega2B/trabajo.uy/EstacionDeTrabajo/src/main/java/logica/Clases/Usuario.java
+++ Entrega2/Entrega2B/trabajo.uy/EstacionDeTrabajo/src/main/java/logica/Clases/Usuario.java
@@ -10,74 +10,52 @@ public abstract class Usuario {
     private String nombre;
     private String apellido;
     private String correo_electronico;
-    
     private String contrasenia;
-
+    private byte[] imagen;
 
     //Getters
-
-
-    public String getNickname() {
-        return nickname;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public String getCorreo_electronico() {
-        return correo_electronico;
-    }
+    public String getNickname() 			{ return nickname; }
+    public String getNombre() 				{ return nombre;     }
+    public String getApellido() 			{ return apellido; }
+    public String getCorreo_electronico() 	{ return correo_electronico; }
+	public String getContraseña() 			{ return contrasenia; }
+	public byte[] getImagen() 				{ return imagen; }
+    
 
     // Setters
+    public void setNickname(String nickname) 						{ this.nickname = nickname; }
+    public void setNombre(String nombre) 							{ this.nombre = nombre; }
+    public void setApellido(String apellido) 						{ this.apellido = apellido; }
+    public void setCorreo_electronico(String correo_electronico) 	{ this.correo_electronico = correo_electronico; }
+    public void setContraseña(String contrasenia) 					{ this.contrasenia = contrasenia; }
+    public void setImagen(byte[] imagen) 							{ this.imagen = imagen; }
 
-
-    public void setNickname(String nickname) {
-        this.nickname = nickname;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public void setCorreo_electronico(String correo_electronico) {
-        this.correo_electronico = correo_electronico;
-    }
-
-    public Usuario(String nickname, String nombre, String apellido, String correo_electronico) {
+    public Usuario(String nickname, String nombre, String apellido, String correo_electronico, String contrasenia, byte[] imagen) {
         this.nickname = nickname;
         this.nombre = nombre;
         this.apellido = apellido; 
         this.correo_electronico = correo_electronico;
+        this.contrasenia = contrasenia;
+        this.imagen = imagen;
     }
 
-    // Operaciones
+    // OPERACIONES
 
     public abstract boolean esEmpresa();
 
     public abstract DTUsuario obtenerDatosUsuario();
+    
+    public abstract DTUsuario obtenerDatosUsuarioEspecial();
+    
+    // NO ESTA EN EL DCD
+    public abstract Set<String> listarOfertasLaborales();
     
     @Override
     public String toString() {
         return nickname + " - " + nombre + " " + apellido;
     }
     
-    public abstract Set<String> listarOfertasLaborales();
+    
 
-	public String getContrasenia() {
-		return contrasenia;
-	}
-
-	public void setContrasenia(String contrasenia) {
-		this.contrasenia = contrasenia;
-	}
     
 }
