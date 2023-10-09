b2990a2c6d855560cf76ebcd242fdbaa5804c112
Nicole R <niky04@gmail.com>
2023-10-08 22:16:12 -0300

    Merge branch 'main' of https://gitlab.fing.edu.uy/tprog/tpgr34.git into main

--- Entrega2/Entrega2B/trabajo.uy/EstacionDeTrabajo/src/main/java/presentacion/AltaOfertaLaboral.java
+++ Entrega2/Entrega2B/trabajo.uy/EstacionDeTrabajo/src/main/java/presentacion/AltaOfertaLaboral.java
@@ -33,7 +33,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashSet;
 import java.awt.event.ActionEvent;
 import javax.swing.JRadioButton;
 
