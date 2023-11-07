4319e29eaa1fe66f24ec98107a2af46d205b22dc
Nicole R <niky04@gmail.com>
2023-11-07 01:48:28 -0300

    postu movil

--- Entrega3/trabajo.uy/ServidorMovil/src/main/java/controller/VerPostulaciones.java
+++ Entrega3/trabajo.uy/ServidorMovil/src/main/java/controller/VerPostulaciones.java
@@ -11,12 +11,14 @@ import logica.servidor.DtUsuario;
 import logica.servidor.ExceptionUsuarioNoEncontrado_Exception;
 import logica.servidor.Servidor;
 import logica.servidor.ServidorService;
+import logica.servidor.TipoUsuarioNoValido_Exception;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.ArrayList;
 
 
-@WebServlet("/ofertaslaborales")
+@WebServlet("/verpostulacion")
 public class VerPostulaciones extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private ServidorService servidorService;
@@ -32,23 +34,43 @@ public class VerPostulaciones extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		String nickname = (String) request.getSession().getAttribute("nickname");
-		
-		
 		List<DtPostulacion> postulaciones = new ArrayList<>();
+		List<DtPostulacion> postulacionOferta = new ArrayList<>();
+		
+
 		try {
 			DtUsuario usuario = (DtUsuario) servidor.obtenerDatosUsuario(nickname);
 			request.setAttribute("usuario", usuario);
+			request.setAttribute("nickname", nickname);
 			List<DtOfertaExtendido> ofertas = servidor.obtenerDTOfertasLaboralesConfirmadas().getOfertasExtendido();
-			request.setAttribute("ofertas", ofertas);
 			postulaciones =  servidor.listarPostulacionesPostulante(nickname).getPostulaciones();
+
+			List<DtOfertaExtendido> ofertasPostulado = new ArrayList<>();
+			
+			for(DtOfertaExtendido offer : ofertas) {
+				
+				boolean existe = servidor.hayPostulacionW(nickname, offer.getNombre()); 
+				
+				if(existe) {
+				 DtPostulacion dtpos = servidor.obtenerDatosPostulacionW(nickname, offer.getNombre());
+ 			     ofertasPostulado.add(offer);
+				 postulacionOferta.add(dtpos);
+				}
+			}
+	
+			request.setAttribute("ofertas", ofertasPostulado);
+			request.setAttribute("postulaciones", postulacionOferta);
 		} catch (ExceptionUsuarioNoEncontrado_Exception exc) {
+			exc.printStackTrace();
+		} catch (TipoUsuarioNoValido_Exception exc) {
+			// TODO Auto-generated catch block
 			exc.printStackTrace();
 		}
 		
 		request.setAttribute("postulaciones", postulaciones);
 		
 		if(!postulaciones.isEmpty()) {
-			request.getRequestDispatcher("/WEB-INF/listarPostula/verpostulaciones.jsp").forward(request, response);
+			request.getRequestDispatcher("/WEB-INF/listarPostula/verpostulacion.jsp").forward(request, response);
 		} else {
 			request.getRequestDispatcher("/WEB-INF/listarPostula/sinpostulaciones.jsp").forward(request, response);
 		}
