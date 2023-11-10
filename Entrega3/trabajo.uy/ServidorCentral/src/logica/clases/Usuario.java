0c0d3cf14755e940e69c801d7fa6185769ac3543
Nicole R <niky04@gmail.com>
2023-11-10 18:27:17 -0300

    cs

--- Entrega3/trabajo.uy/ServidorCentral/src/logica/clases/Usuario.java
+++ Entrega3/trabajo.uy/ServidorCentral/src/logica/clases/Usuario.java
@@ -1,6 +1,14 @@
 package logica.clases;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import logica.Utils;
 import logica.datatypes.DTUsuario;
 
