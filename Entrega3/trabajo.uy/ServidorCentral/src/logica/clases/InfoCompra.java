71ce3fd332ec24551354015e20bbd3ec556d67db
Nicole R <niky04@gmail.com>
2023-11-10 18:26:47 -0300

    cs

--- Entrega3/trabajo.uy/ServidorCentral/src/logica/clases/InfoCompra.java
+++ Entrega3/trabajo.uy/ServidorCentral/src/logica/clases/InfoCompra.java
@@ -2,7 +2,15 @@ package logica.clases;
 
 import excepciones.ExceptionCantidadRestanteDeUnTipoDeOfertaEnUnPaqueteEsNegativa;
 import excepciones.ExceptionValidezNegativa;
-import jakarta.persistence.*;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+//import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import logica.datatypes.DTCantTO;
 import logica.datatypes.DTCompraPaquetes;
 import logica.manejadores.TipoOfertaHandler;
