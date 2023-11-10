e39cae8b2400459c58a563753f02bbb71b370384
Ivan Arriola <ivan.arriola@fing.edu.uy>
2023-11-10 19:04:37 -0300

    visualizar video en iframe

--- Entrega3/trabajo.uy/ServidorWeb/src/main/webapp/js/consultarPostulaciones.js
+++ Entrega3/trabajo.uy/ServidorWeb/src/main/webapp/js/consultarPostulaciones.js
@@ -18,7 +18,7 @@ const cargarInfoPostulante = (nickname, oferta) => {
             $("#motivacion").text(data.motivacion);
             $("#fecha").text(data.fecha);
             if (data.video) {
-                $("#video").html('<a href="' + data.video + '">Ver Video</a>');
+                $("#video").html('<iframe width="100%" height="395" src="' + data.video + '" title="Video Postulacion" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
             } else {
                 $("#video").html('<div class="alert alert-warning" role="alert">No se ha subido un video.</div>');
             }
@@ -29,3 +29,16 @@ const cargarInfoPostulante = (nickname, oferta) => {
     });
 }
 
+/*
+
+Quiero usar este iframe cuando exista data.video
+
+ <iframe
+                                        width="100%"
+                                        height="395"
+                                        src="<%=postulacion.getVideo()%>"
+                                        title="Video Postulacion"
+                                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                                        allowfullscreen>
+                                </iframe>
+ */
