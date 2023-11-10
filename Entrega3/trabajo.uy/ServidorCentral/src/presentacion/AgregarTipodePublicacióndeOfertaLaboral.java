5b0547da3474cd7b64a20c4ddac3ec05c3c82b04
Nicole R <niky04@gmail.com>
2023-11-10 18:41:05 -0300

    cs

--- Entrega3/trabajo.uy/ServidorCentral/src/presentacion/AgregarTipodePublicacióndeOfertaLaboral.java
+++ Entrega3/trabajo.uy/ServidorCentral/src/presentacion/AgregarTipodePublicacióndeOfertaLaboral.java
@@ -10,9 +10,9 @@ import logica.interfaces.ICtrlUsuario;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JDesktopPane;
-import javax.swing.JFrame;
+//import javax.swing.JPanel;
+//import javax.swing.JDesktopPane;
+//import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JLabel;
